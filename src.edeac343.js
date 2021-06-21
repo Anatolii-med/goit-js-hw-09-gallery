ed11e36ca35d3ddff79dcbb41b440644bdf48f6b
Anatolii-med <67787169+Anatolii-med@users.noreply.github.com>
2021-06-21 17:42:16 +0000

    Deploying to gh-pages from @ Anatolii-med/goit-js-hw-09-gallery@5dbde92ac9d8fdb35990694de1830f5b4cb8bb9e 🚀

---
+++ src.edeac343.js
@@ -0,0 +1,46 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
+
+},{"./../images/icon-close.svg":[["icon-close.bb7ed7e5.svg","cAq2"],"cAq2"]}],"DX04":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/1.44f55ba0.jpg";
+},{}],"suED":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/11.4dc6dd74.jpg";
+},{}],"lRKy":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/2.f4a75706.jpg";
+},{}],"GYPS":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/22.bdbea1be.jpg";
+},{}],"WXtp":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/3.b8b21f37.jpg";
+},{}],"NdqY":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/33.6d0b33f8.jpg";
+},{}],"Ctkp":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/4.a60c124c.jpg";
+},{}],"qApi":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/44.4ac2275a.jpg";
+},{}],"x9Gn":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/5.8ee3ce54.jpg";
+},{}],"iKP8":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/55.d561b38a.jpg";
+},{}],"MlJ6":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/6.5efb1638.jpg";
+},{}],"A5e0":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/66.efee508e.jpg";
+},{}],"t1tb":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/7.87e55804.jpg";
+},{}],"HQLz":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/77.452c82c5.jpg";
+},{}],"r6BC":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/8.76a6423c.jpg";
+},{}],"hb3X":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/88.a608328b.jpg";
+},{}],"fY9o":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/9.d786049c.jpg";
+},{}],"DZn9":[function(require,module,exports) {
+module.exports="/goit-js-hw-09-gallery/99.9c78cd4c.jpg";
+},{}],"Vn01":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.galleryItems=void 0;var e=w(require("../images/1.jpg")),i=w(require("../images/11.jpg")),r=w(require("../images/2.jpg")),a=w(require("../images/22.jpg")),g=w(require("../images/3.jpg")),u=w(require("../images/33.jpg")),t=w(require("../images/4.jpg")),p=w(require("../images/44.jpg")),l=w(require("../images/5.jpg")),s=w(require("../images/55.jpg")),o=w(require("../images/6.jpg")),d=w(require("../images/66.jpg")),n=w(require("../images/7.jpg")),f=w(require("../images/77.jpg")),m=w(require("../images/8.jpg")),j=w(require("../images/88.jpg")),q=w(require("../images/9.jpg")),c=w(require("../images/99.jpg"));function w(e){return e&&e.__esModule?e:{default:e}}const v=[{preview:e.default,original:i.default,description:"Hokkaido Flower"},{preview:r.default,original:a.default,description:"Container Haulage Freight"},{preview:g.default,original:u.default,description:"Aerial Beach View"},{preview:t.default,original:p.default,description:"Flower Blooms"},{preview:l.default,original:s.default,description:"Alpine Mountains"},{preview:o.default,original:d.default,description:"Mountain Lake Sailing"},{preview:n.default,original:f.default,description:"Alpine Spring Meadows"},{preview:m.default,original:j.default,description:"Nature Landscape"},{preview:q.default,original:c.default,description:"Lighthouse Coast Sea"}];exports.galleryItems=v;
+},{"../images/1.jpg":"DX04","../images/11.jpg":"suED","../images/2.jpg":"lRKy","../images/22.jpg":"GYPS","../images/3.jpg":"WXtp","../images/33.jpg":"NdqY","../images/4.jpg":"Ctkp","../images/44.jpg":"qApi","../images/5.jpg":"x9Gn","../images/55.jpg":"iKP8","../images/6.jpg":"MlJ6","../images/66.jpg":"A5e0","../images/7.jpg":"t1tb","../images/77.jpg":"HQLz","../images/8.jpg":"r6BC","../images/88.jpg":"hb3X","../images/9.jpg":"fY9o","../images/99.jpg":"DZn9"}],"chxP":[function(require,module,exports) {
+"use strict";var e=require("./images");const t=document.querySelector(".gallery"),n=e.galleryItems.map(({preview:e,original:t,description:n})=>`<li class="gallery__item">\n        <a\n          class="gallery__link"\n          href="${t}"\n        >\n          <img\n            class="gallery__image"\n            src="${e}"\n            data-source="${t}"\n            alt="${n}"\n          />\n        </a>\n      </li>`);t.insertAdjacentHTML("beforeend",n.join(""));const s=document.querySelector("div.lightbox"),r=e=>{if(e.preventDefault(),"IMG"===e.target.nodeName)return s.classList.add("is-open")};t.addEventListener("click",r);const i=document.querySelector(".lightbox__image"),c=e=>{if(e.preventDefault(),s.classList.contains("is-open")&&"IMG"===e.target.nodeName){const t=e.target.dataset.source;return i.src=t}};t.addEventListener("click",c);const a=document.querySelector(".lightbox__button"),o=()=>{if(s.classList.contains("is-open"))return i.src="",s.classList.remove("is-open")};a.addEventListener("click",o);const l=document.querySelector("div.lightbox__overlay");l.addEventListener("click",o);const d=function(e){if("Escape"===e.key)return i.src="",s.classList.remove("is-open")};window.addEventListener("keydown",d);
+},{"./images":"Vn01"}],"Focm":[function(require,module,exports) {
+"use strict";require("./sass/main.scss"),require("./js/images"),require("./js/gallery");
+},{"./sass/main.scss":"clu1","./js/images":"Vn01","./js/gallery":"chxP"}]},{},["Focm"], null)
+//# sourceMappingURL=/goit-js-hw-09-gallery/src.edeac343.js.map
