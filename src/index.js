f8a78dabe7d85737de4944fc8a5904b3dc114180
Anatolii-med <anatolii@github.com>
2021-06-21 14:48:07 +0300

    images

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './sass/main.scss';
 
 const galleryItems = [
   {
-    preview: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    preview: './images/1.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
     description: 'Hokkaido Flower',
